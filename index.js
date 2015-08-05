72dcd4e84b58f9de46bbf2e1cde9b230c6258f0e
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2015-08-05 16:24:29 +1000

    Refactored index entry point for modern ember-cli loadng

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 /* jshint node: true */
 'use strict';
-var path = require('path');
-var Funnel = require('broccoli-funnel');
 
 module.exports = {
   name: 'ember-spin-button',
 
-  blueprintsPath: function() {
-    return path.join(__dirname, 'blueprints');
-  },
-
-  treeForStyles: function() {
-    var stylesheetPath = path.resolve(__dirname, 'addon', 'spin-button');
-    var stylesheetsTree = new Funnel(this.treeGenerator(stylesheetPath), {
-      srcDir: 'styles',
-      destDir: '/app/styles'
-    });
-    return stylesheetsTree;
-  },
-
   included: function(app) {
     this._super.included(app);
     app.import(app.bowerDirectory + '/spin.js/spin.js');
-  },
+  }
 };
