f9c2416fed0b74b7a4cf492141f452f7c5a95585
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2015-08-22 15:45:16 +1000

    Fixed assets using Broccoli funnel

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+
 module.exports = {
   name: 'ember-spin-button',
 
@@ -8,5 +11,15 @@ module.exports = {
     this._super.included(app);
 
     app.import(app.bowerDirectory + '/spin.js/spin.js');
+  },
+
+  treeForStyles: function() {
+    var stylesPath = path.join(__dirname, 'addon');
+    var stylesTree = new Funnel(this.treeGenerator(stylesPath), {
+      srcDir: './styles',
+      destDir: '/app/styles'
+    });
+    return stylesTree;
   }
+
 };
